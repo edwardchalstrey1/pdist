97d87bf6f06de66a1e4a94dcbf107669e68230e2
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-04-25 15:42:30 +0100

    added license to RubyGems.org

--- pdist.gemspec
+++ pdist.gemspec
@@ -1,7 +1,7 @@
 #encoding: utf-8
 Gem::Specification.new do |s|
   s.name        = 'pdist'
-  s.version     = '0.0.2'
+  s.version     = '0.0.3'
   s.date        = '2014-04-16'
   s.summary     = "Permutation distance metrics"
   s.description = "Ruby gem to compare permutations using distance measures/metrics. github: https://github.com/edwardchalstrey1/pdist"
@@ -9,5 +9,5 @@ Gem::Specification.new do |s|
   s.email       = 'edwardchalstrey@gmail.com'
   s.files       = ["lib/pdist.rb"]
   s.homepage    = 'http://rubygems.org/gems/pdist'
-  s.license     = 'none'
+  s.license     = 'MIT'
 end
---
+++ test/pdist_unit_test.rb
@@ -0,0 +1,40 @@
+#encoding: utf-8
+require 'pdist'
+require 'test/unit'
+
+class TestPDist < Test::Unit::TestCase
+
+	A = %w(a b c)
+	C = A.reverse
+
+	def test_deviation
+		assert_equal(0, PDist.deviation(A,A))
+		assert_equal(1, PDist.deviation(A,C))
+		assert_equal(1, PDist.deviation(%w(a b), %w(b a)))
+	end
+
+	def test_square
+		assert_equal(0, PDist.square(A,A))
+		assert_equal(1, PDist.square(A,C))
+	end
+
+	def test_hamming
+		assert_equal(0, PDist.hamming(A,A))
+		assert_equal(1, PDist.hamming(A, %w(b c a)))
+	end
+
+	def test_rdist
+		assert_equal(0, PDist.rdist(A,A))
+		assert_equal(1, PDist.rdist(A,C))
+	end
+
+	def test_lcs
+		assert_equal(0, PDist.lcs(A,A))
+		assert_equal(1, PDist.lcs(A,C))
+	end
+
+	def test_kendalls_tau
+		assert_equal(0, PDist.kendalls_tau(A,A))
+		assert_equal(1, PDist.kendalls_tau(A,C))
+	end
+end
