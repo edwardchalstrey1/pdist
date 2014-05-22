478a23c06ad760880c1fb5d77b2496aa2a675ce1
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-05-22 16:26:18 +0100

    changed constants to instance variables in test

--- test/test_pdist.rb
+++ test/test_pdist.rb
@@ -4,50 +4,52 @@ require 'test/unit'
 
 class TestPDist < Test::Unit::TestCase
 
-	A = %w(a b c d)
-	B = A.reverse
-	C = %w(b d c a)
+	def setup
+		@a = %w(a b c d)
+		@b = @a.reverse
+		@c = %w(b d c a)
+	end
 
 	def test_distances
-		assert_equal([0,0,0,0], PDist.distances(A,A))
-		assert_equal([3,1,1,3], PDist.distances(A,B))
-		assert_equal([3,1,0,2], PDist.distances(A,C))
+		assert_equal([0,0,0,0], PDist.distances(@a,@a))
+		assert_equal([3,1,1,3], PDist.distances(@a,@b))
+		assert_equal([3,1,0,2], PDist.distances(@a,@c))
 	end
 
 	def test_deviation
-		assert_equal(0, PDist.deviation(A,A))
-		assert_equal(1, PDist.deviation(A,B))
-		assert_equal(6.0/8.0, PDist.deviation(A,C))
+		assert_equal(0, PDist.deviation(@a,@a))
+		assert_equal(1, PDist.deviation(@a,@b))
+		assert_equal(6.0/8.0, PDist.deviation(@a,@c))
 	end
 
 	def test_square
-		assert_equal(0, PDist.square(A,A))
-		assert_equal(1, PDist.square(A,B))
-		assert_equal(4.2/6.0, PDist.square(A,C))
+		assert_equal(0, PDist.square(@a,@a))
+		assert_equal(1, PDist.square(@a,@b))
+		assert_equal(4.2/6.0, PDist.square(@a,@c))
 	end
 
 	def test_hamming
-		assert_equal(0, PDist.hamming(A,A))
-		assert_equal(1, PDist.hamming(A,B))
-		assert_equal(0.75, PDist.hamming(A,C))
+		assert_equal(0, PDist.hamming(@a,@a))
+		assert_equal(1, PDist.hamming(@a,@b))
+		assert_equal(0.75, PDist.hamming(@a,@c))
 	end
 
 	def test_rdist
-		assert_equal(0, PDist.rdist(A,A))
-		assert_equal(1, PDist.rdist(A,B))
-		assert_equal(1, PDist.rdist(A,C))
-		assert_equal(2.0/3.0, PDist.rdist(A,%w(a b d c)))
+		assert_equal(0, PDist.rdist(@a,@a))
+		assert_equal(1, PDist.rdist(@a,@b))
+		assert_equal(1, PDist.rdist(@a,@c))
+		assert_equal(2.0/3.0, PDist.rdist(@a,%w(a b d c)))
 	end
 
 	def test_lcs
-		assert_equal(0, PDist.lcs(A,A))
-		assert_equal(1, PDist.lcs(A,B))
-		assert_equal(2.0/3.0, PDist.lcs(A,C))
+		assert_equal(0, PDist.lcs(@a,@a))
+		assert_equal(1, PDist.lcs(@a,@b))
+		assert_equal(2.0/3.0, PDist.lcs(@a,@c))
 	end
 
 	def test_kendalls_tau
-		assert_equal(0, PDist.kendalls_tau(A,A))
-		assert_equal(1, PDist.kendalls_tau(A,B))
-		assert_equal(2.0/3.0, PDist.kendalls_tau(A,C))
+		assert_equal(0, PDist.kendalls_tau(@a,@a))
+		assert_equal(1, PDist.kendalls_tau(@a,@b))
+		assert_equal(2.0/3.0, PDist.kendalls_tau(@a,@c))
 	end
 end
